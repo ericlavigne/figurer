39352ed1d1da8af394c0584378ec3edd93503e77
Eric Lavigne <lavigne.eric@gmail.com>
2018-10-01 22:18:41 -0400

    Create internal namespace to clarify which functions are private
    Moved several functions from figurer.core to figurer.internal to
    clarify which functions should not be called from outside the
    figurer library.

--- src/figurer/core.clj
+++ src/figurer/core.clj
@@ -1,5 +1,6 @@
 (ns figurer.core
-  (:require [incanter.distributions :refer [draw]]))
+  (:require [figurer.internal :refer :all]
+            [incanter.distributions :refer [draw]]))
 
 (defn define-problem
   "Defines the context for all simulation and optimization.
@@ -32,105 +33,6 @@
       {:nodes-so-far 1
        :initial-node-id initial-node-id
        :node-id-to-node {initial-node-id initial-node}})))
-
-(defn figure-next-node
-  "Private: Choose next node (or return nil to create new node).
-  
-   TODO: Choose promising node (or nil if winner so far is clear)
-         rather than just choosing randomly."
-  [context node-id]
-  (draw (conj (keys
-                (:next-nodes (get (:node-id-to-node context)
-                                  node-id)))
-          nil)))
-
-(defn figure-rollout
-  "Private: Reached end of nodes - follow random policy to determine end result.
-   Return average value of remaining states."
-  [context node-id remaining-depth]
-  (if (< remaining-depth 1) 0.0
-    (let [start-node (get (:node-id-to-node context) node-id)
-          start-actuation (mapv draw (:policy start-node))
-          start-state ((:predict context) (:state start-node) start-actuation)]
-      (loop [remaining remaining-depth
-             total 0.0
-             state start-state]
-        (let [new-total (+ total ((:value context) state))]
-          (if (<= remaining 1)
-            (/ new-total remaining-depth)
-            (recur (dec remaining) new-total
-              ((:predict context)
-               state
-               (mapv draw ((:policy context) state))))))))))
-  
-(defn figure-create-node
-  "Private: Create new node following node-id.
-   Return modified context and created node ID."
-  [context parent-node-id]
-  (let [parent (get (:node-id-to-node context) parent-node-id)
-        actuation (mapv draw (:policy parent))
-        state ((:predict context) (:state parent) actuation)
-        policy ((:policy context) state)
-        node-id (:nodes-so-far context)
-        direct-value ((:value context) state)
-        new-node {:id node-id
-                  :state state
-                  :policy policy
-                  :goal :maximize
-                  :visits 0
-                  :direct-value direct-value
-                  :value direct-value
-                  :next-nodes {}}
-        new-context (assoc-in
-                      (update context :nodes-so-far inc)
-                      [:node-id-to-node node-id]
-                      new-node)
-        new-context (assoc-in new-context
-                      [:node-id-to-node parent-node-id :next-nodes node-id]
-                      {:node-id node-id :actuation actuation})]
-    [new-context node-id]))
-
-(defn figure-once
-  "Private: Perform one rollout to collect statistics."
-  [context]
-  (let [visited-node-ids (vec (take-while identity
-                                (iterate #(figure-next-node context %)
-                                  (:initial-node-id context))))
-        [context new-node-id] (if (>= (count visited-node-ids) (:depth context))
-                                [context nil]
-                                (figure-create-node context (last visited-node-ids)))
-        visited-node-ids (if new-node-id
-                           (conj visited-node-ids new-node-id)
-                           visited-node-ids)
-        depth-remaining (- (:depth context) (count visited-node-ids))
-        value-after-nodes (figure-rollout context (last visited-node-ids) depth-remaining)
-        total-after-nodes (* depth-remaining value-after-nodes)
-        context (update-in context [:node-id-to-node (last visited-node-ids)]
-                  (fn [node]
-                    (merge node
-                      {:value (/ (+ (:direct-value node)
-                                    total-after-nodes)
-                                 (inc depth-remaining))
-                       :visits (inc (:visits node))})))
-        context (loop [node-ids-to-update (drop 1 (reverse visited-node-ids))
-                       context context]
-                  (if (empty? node-ids-to-update)
-                    context
-                    (let [node-id (first node-ids-to-update)
-                          node (get (:node-id-to-node context) node-id)
-                          children (map (:node-id-to-node context)
-                                     (keys (:next-nodes node)))
-                          max-child-value (apply max (map :value children))
-                          depth-after-node (- (:depth context) (count node-ids-to-update))
-                          new-value (/ (+ (:direct-value node)
-                                          (* max-child-value depth-after-node))
-                                       (inc depth-after-node))
-                          new-node (merge node
-                                     {:value new-value
-                                      :visits (inc (:visits node))})]
-                      (recur (rest node-ids-to-update)
-                        (assoc-in context [:node-id-to-node node-id] new-node)))))]
-    context))
 
 (defn figure
   "Perform optimization, returning a more optimized context."
---
+++ src/figurer/internal.clj
@@ -0,0 +1,104 @@
+(ns figurer.internal
+  "Internal functions that should not be called from outside the figurer library.
+   Functions in this namespace may be changed or removed without notice."
+  (:require [incanter.distributions :refer [draw]]))
+
+(defn figure-next-node
+  "Private: Choose next node (or return nil to create new node).
+  
+   TODO: Choose promising node (or nil if winner so far is clear)
+         rather than just choosing randomly."
+  [context node-id]
+  (draw (conj (keys
+                (:next-nodes (get (:node-id-to-node context)
+                                  node-id)))
+          nil)))
+
+(defn figure-rollout
+  "Private: Reached end of nodes - follow random policy to determine end result.
+   Return average value of remaining states."
+  [context node-id remaining-depth]
+  (if (< remaining-depth 1) 0.0
+    (let [start-node (get (:node-id-to-node context) node-id)
+          start-actuation (mapv draw (:policy start-node))
+          start-state ((:predict context) (:state start-node) start-actuation)]
+      (loop [remaining remaining-depth
+             total 0.0
+             state start-state]
+        (let [new-total (+ total ((:value context) state))]
+          (if (<= remaining 1)
+            (/ new-total remaining-depth)
+            (recur (dec remaining) new-total
+              ((:predict context)
+               state
+               (mapv draw ((:policy context) state))))))))))
+  
+(defn figure-create-node
+  "Private: Create new node following node-id.
+   Return modified context and created node ID."
+  [context parent-node-id]
+  (let [parent (get (:node-id-to-node context) parent-node-id)
+        actuation (mapv draw (:policy parent))
+        state ((:predict context) (:state parent) actuation)
+        policy ((:policy context) state)
+        node-id (:nodes-so-far context)
+        direct-value ((:value context) state)
+        new-node {:id node-id
+                  :state state
+                  :policy policy
+                  :goal :maximize
+                  :visits 0
+                  :direct-value direct-value
+                  :value direct-value
+                  :next-nodes {}}
+        new-context (assoc-in
+                      (update context :nodes-so-far inc)
+                      [:node-id-to-node node-id]
+                      new-node)
+        new-context (assoc-in new-context
+                      [:node-id-to-node parent-node-id :next-nodes node-id]
+                      {:node-id node-id :actuation actuation})]
+    [new-context node-id]))
+
+(defn figure-once
+  "Private: Perform one rollout to collect statistics."
+  [context]
+  (let [visited-node-ids (vec (take-while identity
+                                (iterate #(figure-next-node context %)
+                                  (:initial-node-id context))))
+        [context new-node-id] (if (>= (count visited-node-ids) (:depth context))
+                                [context nil]
+                                (figure-create-node context (last visited-node-ids)))
+        visited-node-ids (if new-node-id
+                           (conj visited-node-ids new-node-id)
+                           visited-node-ids)
+        depth-remaining (- (:depth context) (count visited-node-ids))
+        value-after-nodes (figure-rollout context (last visited-node-ids) depth-remaining)
+        total-after-nodes (* depth-remaining value-after-nodes)
+        context (update-in context [:node-id-to-node (last visited-node-ids)]
+                  (fn [node]
+                    (merge node
+                      {:value (/ (+ (:direct-value node)
+                                    total-after-nodes)
+                                 (inc depth-remaining))
+                       :visits (inc (:visits node))})))
+        context (loop [node-ids-to-update (drop 1 (reverse visited-node-ids))
+                       context context]
+                  (if (empty? node-ids-to-update)
+                    context
+                    (let [node-id (first node-ids-to-update)
+                          node (get (:node-id-to-node context) node-id)
+                          children (map (:node-id-to-node context)
+                                     (keys (:next-nodes node)))
+                          max-child-value (apply max (map :value children))
+                          depth-after-node (- (:depth context) (count node-ids-to-update))
+                          new-value (/ (+ (:direct-value node)
+                                          (* max-child-value depth-after-node))
+                                       (inc depth-after-node))
+                          new-node (merge node
+                                     {:value new-value
+                                      :visits (inc (:visits node))})]
+                      (recur (rest node-ids-to-update)
+                        (assoc-in context [:node-id-to-node node-id] new-node)))))]
+    context))
+
