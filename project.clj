6d686f342d3c82f9ef747f4db08f0616d3ad70e0
Eric Lavigne <lavigne.eric@gmail.com>
2018-10-13 23:25:21 -0400

    Upgrade to Clojure 1.10.0-beta3
    Specified latest Clojure version only as dev dependency.

--- project.clj
+++ project.clj
@@ -4,4 +4,5 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[frenet "0.1.0"]
-                 [incanter/incanter-core "1.9.3"]])
+                 [incanter/incanter-core "1.9.3"]]
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.10.0-beta3"]]}})
